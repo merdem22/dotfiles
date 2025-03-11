81a7f6273498abcecc79d87737c30025c9017e44
merdem22 <merdem22@ku.edu.tr>
2025-03-11 21:32:48 +0300

    Initial commit of .config files

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,44 @@
+local wezterm = require("wezterm")
+
+config = wezterm.config_builder()
+
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE", --disable the title bar but enable the resizable border
+	default_cursor_style = "BlinkingBar",
+	color_scheme = "Nord (Gogh)",
+	font = wezterm.font("JetBrains Mono", { weight = "Bold"}),
+	font_size = 12.5,
+	background = {
+		{
+			source = {
+				File = "/Users/merterdem/Images/SequoiaDark.png"
+			},
+			hsb = {
+				hue = 1.0,
+				saturation = 1.02,
+				brightness = 0.25,
+			},
+			width = "100%",
+			height = "100%",
+		},
+		{
+			source = {
+				Color = "#282c35",
+			},
+			width = "100%",
+			height = "100%",
+			opacity = 0.55,
+		},
+	},
+	window_padding = {
+		left = 6,
+		right = 6,
+		top = 3,
+		bottom = 3,
+	},
+}
+
+return config
